05fa2c9cf5bb96e480ea46cd854d11e1d86da87c
Ramsest01 <56605629+Ramsest01@users.noreply.github.com>
2020-10-28 13:20:19 -0400

    update conexion

--- coneccion/coneccion.php
+++ coneccion/coneccion.php
@@ -1,9 +1,9 @@
 <?php  
-	$user = 'postgres';
-	$passwd = '';
-	$db = 'backoffice';
+	$user = 'iprhayuzjjvjle';
+	$passwd = 'a031e7bfdba6ffe6e4002e5f10aab60716cc88c5152b4cfb68771327312081ca';
+	$db = 'd789e39p2pck55';
 	$port = 5432;
-	$host = 'localhost';
+	$host = 'ec2-34-237-89-96.compute-1.amazonaws.com';
 	$strCnx = "host=$host port=$port dbname=$db user=$user password=$passwd";
 	$cnx = pg_connect($strCnx) or die ("Error de conexion. ". pg_last_error());
 
